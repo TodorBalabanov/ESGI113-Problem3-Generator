e22eae2c69c29232b5e543a77304a81251c1c83b
Todor Balabanov <todor.balabanov@gmail.com>
2015-09-12 16:24:33 +0300

    Initial revision.

---
+++ Problem3ESGI113.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+	int X;
+	int Y;
+	int n;
+	int i;
+	int maxPieceArea;
+	int minSide = 30;
+
+	srand( time(NULL) );
+
+	X = 1000 + rand() % 1;
+	Y = 15000 + rand() % 1;
+
+	n = 700 + rand() % 101;
+
+	maxPieceArea = X * Y / n;
+
+	printf("%d", n);
+	printf("\r\n");
+
+	printf("%d", X);
+	printf(" ");
+	printf("%d", Y);
+	printf("\r\n");
+
+	for(i=0; i<n; i++) {
+		int a = minSide + rand() % (X-minSide+1);
+		int b = minSide + rand() % (Y-minSide+1);
+
+		while(a*b > maxPieceArea) {
+			a = minSide + rand() % (X-minSide+1);
+			b = minSide + rand() % (Y-minSide+1);
+		}
+
+		printf("%d", a);
+		printf(" ");
+		printf("%d", b);
+		printf("\r\n");
+	}
+
+	return 0;
+}
+
